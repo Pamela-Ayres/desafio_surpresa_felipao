21e98653b1759cb9b213a07058663d485ea1c2a2
Pam <165807628+Pamela-Ayres@users.noreply.github.com>
2024-04-13 01:06:05 -0300

    Add files via upload

---
+++ Desafiosupresa.js
@@ -0,0 +1,25 @@
+//DESAFIO SUPRESA - FELIPÃO
+let nomepokemon = (["Poochyena1", "Poochyena2","Zigszagoon", "Dragonite1", "Dragonite2", "Dragonite3", "Hurhple"])
+let timepokemonFemenino = (["Zigszagoon", "Dragonite2", "Dragonite3","Poochyena2"])
+let timepokemonMasculino = (["Poochyena1", "Dragonite1", "Hurhple" ]) 
+//CADASTRO POKEMON//
+console.log(nomepokemon[0],"Cadastrado com sucesso!")
+console.log(nomepokemon[1],"Cadastrado com sucesso!")
+console.log(nomepokemon[2],"Cadastrado com sucesso!")
+console.log(nomepokemon[3],"Cadastrado com sucesso!")
+console.log(nomepokemon[4],"Cadastrado com sucesso!")
+console.log(nomepokemon[5],"Cadastrado com sucesso!")
+console.log(nomepokemon[6],"Cadastrado com sucesso!")
+//TIME POKEMON FEMENINO
+console.log("Os Participantes do time Pokemon Femenino são: "  +  timepokemonFemenino)
+//TIME POKEMON MASCULINO
+console.log("Os participantes do time Pokemon Masculino são: "  +  timepokemonMasculino)
+//LEVEL POKEMON
+let levelPokemon2 = (["Poochyena1", "Zigszagoon", "Hurhple"])
+let levelPokemon3 = ("Poochyena2")
+let levelPokemon5 = (["Dragonite1","Dragonite2", "Dragonite3"])
+console.log("Qual Pokemon está no lével 2: " + levelPokemon2)
+console.log("Qual Pokemon está no lével 3: " + levelPokemon3)
+console.log("Qual Pokemon está no lével 5: " + levelPokemon5)
+//AGRADECIMENTO
+console.log("Obrigada Felipão, sua didática é compreeniva, estou feliz por completar este desafio o/")
